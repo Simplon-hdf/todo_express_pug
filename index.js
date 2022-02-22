a357c0227cc99f28ff81273c346cfaaee4f479da
Hamid Berkaine <hamiddev78@gmail.com>
2022-02-22 15:18:27 +0100

    add route to contact

--- index.js
+++ index.js
@@ -5,20 +5,16 @@ const port = 3000;
 
 app.set('view engine', 'pug');
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.get('/about', (req, res) => {
-//   res.send('Hello World! to about')
-// })
-
 app.get('/', (req, res) => {
   res.render('index', { title: 'Hey', message: 'Hello everyone!' })
 })
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'Yo', message1: 'Hamid', message2: 'Danny', message3: 'Christopher'} )
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact', message: 'Welcome for you', message1: 'Hey everyone we are developers', messageFooter: 'Nous somme dans le footer'} )
 })
 
 app.listen(port, () => {
